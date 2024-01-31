75d98142bcc4166c11d22b70e91eba851a03bfc8
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2024-01-31 14:29:45 +0000

    Add Audio support

--- yt/download.sh
+++ yt/download.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 export PYTHONPATH="${PYTHONPATH}:/yt/"
-VID_URL=$1
+VID_URL="$1"
+VID_TITLE=$(python -m youtube_dl --get-title "$VID_URL" | sed 's/[\[\]!@#\$%^&*()_+|~=`{}\\:;"'\''<>?,.\/]/_/g')
 VID_TITLE=$(python -m youtube_dl --get-title "$VID_URL" | sed 's/[/\:*?"<>|]/_/g')
-python -m youtube_dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o "${VID_TITLE}.mp4" --merge-output-format mp4 "$VID_URL" || exit 1
+COMMAND_TO_EXECUTE="python -m youtube_dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '${VID_TITLE}.mp4' --merge-output-format mp4 '$VID_URL'"
+
+# Check the environment variable if IS_AUDIO_ONLY is set then download the audio only
+if [ "$IS_AUDIO_ONLY" = "true" ]; then
+    COMMAND_TO_EXECUTE="python -m youtube_dl -x --audio-format mp3 -f 'bestaudio/best' -o '${VID_TITLE}.mp3' '$VID_URL'"
+fi
+
+# Execute the command
+eval "$COMMAND_TO_EXECUTE" || exit 1
 
 # Start the bot server in the background
-/usr/local/bin/telegram-bot-api --local --http-port 3000 --api-id $API_ID --api-hash $API_HASH &
+/usr/local/bin/telegram-bot-api --local --http-port 3000 --api-id "$API_ID" --api-hash "$API_HASH" &
 
 # Wait for the server to start
 while ! nc -z localhost 3000; do   
@@ -13,4 +22,10 @@ while ! nc -z localhost 3000; do
 done
 
 # Send a CURL request to the bot with the video
-curl --location "http://localhost:3000/bot${BOT_TOKEN}/sendVideo?chat_id=${CHAT_ID}" --form "video=@\"${VID_TITLE}.mp4\"" || exit 1
+if [ "$IS_AUDIO_ONLY" = "true" ]; then
+    CURL_COMMAND_TO_EXECUTE="curl --location 'http://localhost:3000/bot${BOT_TOKEN}/sendAudio?chat_id=${CHAT_ID}' --form 'audio=@\"${VID_TITLE}.mp3\"'"
+else
+    CURL_COMMAND_TO_EXECUTE="curl --location 'http://localhost:3000/bot${BOT_TOKEN}/sendVideo?chat_id=${CHAT_ID}' --form 'video=@\"${VID_TITLE}.mp4\"'"
+fi
+
+eval "$CURL_COMMAND_TO_EXECUTE" || exit 1
